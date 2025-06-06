a1087e2e4a2474b15d73be8bb5b07aeb64f3f451
psychzfrank <frank.t@psychz.net>
2025-06-06 14:36:07 -0700

    Update 1_setup_dcops.sh
    comment out add operator1. try to run as dcops user

--- 1_setup_dcops.sh
+++ 1_setup_dcops.sh
@@ -42,9 +42,9 @@ run_cmd "Check UFW status" ufw status
 run_cmd "Enable UFW firewall" ufw --force enable
 
 # Add operator1 user
-run_cmd "Add user operator1" adduser --gecos "" operator1
-run_cmd "Set password for operator1" bash -c "echo 'operator1:operator1' | chpasswd"
-run_cmd "Add operator1 to dialout group" adduser operator1 dialout
+# run_cmd "Add user operator1" adduser --gecos "" operator1
+# run_cmd "Set password for operator1" bash -c "echo 'operator1:operator1' | chpasswd"
+# run_cmd "Add operator1 to dialout group" adduser operator1 dialout
 
 # Install screen utility
 run_cmd "Install screen package" apt install -y screen
